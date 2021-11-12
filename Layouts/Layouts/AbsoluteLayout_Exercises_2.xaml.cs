1c6a04a8840fb5d5bbe1b138d0aa48bef7407309
linhdo <linhlee88999@gmai.com>
2021-11-12 14:02:02 +0700

    Exercise in Layouts

---
+++ Layouts/Layouts/AbsoluteLayout_Exercises_2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Layouts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbsoluteLayout_Exercises_2 : ContentPage
+    {
+        public AbsoluteLayout_Exercises_2()
+        {
+            InitializeComponent();
+        }
+    }
+}
