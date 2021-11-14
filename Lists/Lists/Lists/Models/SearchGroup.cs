868dce928585506caed075117e3e3285c04965ef
linhdo <linhlee88999@gmai.com>
2021-11-14 22:12:17 +0700

    Exercise on Lists

---
+++ Lists/Lists/Lists/Models/SearchGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Lists.Models
+{
+    public class SearchGroup : List<Search>
+    {
+        IEnumerable<Search> searches;
+        public string Title { get; set; }
+
+        public SearchGroup( IEnumerable<Search> searches):base(searches)
+        {
+            Title = "Recent Searches";
+        }
+    }
+}
