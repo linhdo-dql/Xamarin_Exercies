33ba4563ce9e3ca9cdf3097893ed0a1da72cc28e
linhdo <linhlee88999@gmai.com>
2021-11-15 17:18:45 +0700

    Exercise on Navigation

---
+++ Navigation/Navigation/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Navigation.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl {
+            get
+            {
+                return "https://lorempixel.com/200/200/people/" + this.Id +"/";
+            }
+        }
+    }
+}
---
+++ Navigation/Navigation/UserProfilePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using InstagramApp;
+using Navigation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserProfilePage : ContentPage
+    {
+        UserService userService = new UserService();
+        public UserProfilePage(Activity activity)
+        {
+            InitializeComponent();
+            if(activity == null)
+            {
+                throw new ArgumentNullException(nameof(activity));
+            }
+            User user = userService.GetUser(activity.UserId);
+            BindingContext = user;
+        }
+    }
+}
