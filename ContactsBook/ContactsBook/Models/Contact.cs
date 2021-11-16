ad44db1c421fc88cd4e77c332597d011c7457bf4
linhdo <linhlee88999@gmai.com>
2021-11-16 15:29:18 +0700

    Exercise in Forms and Setting Pages

---
+++ ContactsBook/ContactsBook/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ContactsBook.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool IsBlock     { get; set; }
+
+        public string FullName => BehaviorName(FirstName)+ " " + LastName;
+        public string BehaviorName( string str )
+        {
+            var arrStr = str.Split(' ');
+            return arrStr[0];
+        }
+    }
+}
